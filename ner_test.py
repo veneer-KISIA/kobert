6caf5d25a3bef288b056b8e26eacfe6577b74cc4
ScobraCK <93692827+ScobraCK@users.noreply.github.com>
2023-09-10 11:05:44 +0900

    Update ner_test.py

--- ner_test.py
+++ ner_test.py
@@ -15,4 +15,4 @@ def get_predict(text):
     return predict(pred_config)
 
 if __name__ == "__main__":
-    print(get_predict('안녕하세요 단국대학교 학생 강인창입니다.'))
+    print(get_predict('안녕하세요 단국대학교 학생 홍길동입니다.'))
